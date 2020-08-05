513231a4c7fe0319f42a65a3822729201635b5cb
Dev_Khatab <khatap1@hotmail.com>
2020-08-05 01:50:12 -0700

    Returning Pagination Metadata

--- CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
+++ CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
@@ -7,7 +7,7 @@ namespace CourseLibrary.API.ResourceParameters
 {
     public class AuthorsResourceParameters
     {
-        const int maxPageSize = 20;
+        private const int MaxPageSize = 20;
         public string MainCategory { get; set; }
         public string SearchQuery { get; set; }
         public int PageNumber { get; set; } = 1;
@@ -16,7 +16,7 @@ namespace CourseLibrary.API.ResourceParameters
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
 
     }
