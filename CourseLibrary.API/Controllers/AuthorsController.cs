87f3a72c487a2c3d18305544d5995437bc496579
Dev_Khatab <khatap1@hotmail.com>
2020-08-29 14:31:10 -0700

    Taking consumer errors into account when sorting

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using CourseLibrary.API.Entities;
 
 namespace CourseLibrary.API.Controllers
 {
@@ -16,14 +17,17 @@ namespace CourseLibrary.API.Controllers
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
         private readonly IMapper _mapper;
+        private readonly IPropertyMappingService _propertyMappingService;
 
         public AuthorsController(ICourseLibraryRepository courseLibraryRepository,
-            IMapper mapper)
+            IMapper mapper, IPropertyMappingService propertyMappingService)
         {
             _courseLibraryRepository = courseLibraryRepository ??
                 throw new ArgumentNullException(nameof(courseLibraryRepository));
             _mapper = mapper ??
                 throw new ArgumentNullException(nameof(mapper));
+            _propertyMappingService = propertyMappingService ??
+                throw new ArgumentNullException(nameof(propertyMappingService));
         }
 
         [HttpGet(Name = "GetAuthors")]
@@ -31,6 +35,12 @@ namespace CourseLibrary.API.Controllers
         public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
             [FromQuery] AuthorsResourceParameters authorsResourceParameters)
         {
+            if (!_propertyMappingService.ValidMappingExistsFor<AuthorDto,Author>
+                (authorsResourceParameters.OrderBy))
+            {
+                return BadRequest();
+
+            }
             var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
             var previousPageLink = authorsFromRepo.HasPrevious
                 ? CreateAuthorsResourceUri(authorsResourceParameters,
--- CourseLibrary.API/Services/IPropertyMappingService.cs
+++ CourseLibrary.API/Services/IPropertyMappingService.cs
@@ -5,6 +5,8 @@ namespace CourseLibrary.API.Services
     public interface IPropertyMappingService
     {
         Dictionary<string, PropertyMappingValue> GetPropertyMapping
-            <TSource, TDestination>(); 
+            <TSource, TDestination>();
+
+        bool ValidMappingExistsFor<TSource, TDestination>(string fields);
     }
 }
